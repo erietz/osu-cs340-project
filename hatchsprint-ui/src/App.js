d2b470c12d53b0617cffbadeec46107ec0226c41
Ethan Rietz <ewrietz@gmail.com>
2021-10-26 15:31:00 -0500

    add RestaurantPage

--- hatchsprint-ui/src/App.js
+++ hatchsprint-ui/src/App.js
@@ -1,14 +1,21 @@
 import './App.css';
 import HomePage from './pages/HomePage.js';
+import RestaurantPage from './pages/RestaurantPage.js';
 import { BrowserRouter as Router, Route} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
         <Router>
+
             <Route path="/" exact>
                 <HomePage/>
             </Route>
+
+            <Route path="/restaurants" exact>
+                <RestaurantPage/>
+            </Route>
+
         </Router>
     </div>
   );
---
+++ hatchsprint-ui/src/pages/RestaurantPage.js
@@ -0,0 +1,7 @@
+export default function RestaurantPage() {
+    return (
+        <>
+            <h1>Restaurant Stuff Here</h1>
+        </>
+    )
+}
