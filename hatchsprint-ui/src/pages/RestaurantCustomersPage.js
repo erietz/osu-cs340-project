255f70f4f732ca07fd4bea7a1018efd8fa564d85
Ethan Rietz <ewrietz@gmail.com>
2021-12-01 21:38:04 -0600

    add search functionality for restaurantcustomers

--- hatchsprint-ui/src/pages/RestaurantCustomersPage.js
+++ hatchsprint-ui/src/pages/RestaurantCustomersPage.js
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import SideBar from '../components/Sidebar.js';
 import SearchRestaurantCustomers from '../components/SearchRestaurantCustomers.js';
 import RestaurantCustomersTable from '../components/RestaurantCustomersTable.js';
+import RestaurantDataList from '../components/RestaurantDataList.js';
 
 export default function RestaurantCustomersPage() {
     const [tableData, setTableData] = useState([]);
@@ -22,13 +23,37 @@ export default function RestaurantCustomersPage() {
             .catch(err => console.error(err));
     }, [])
 
+    const [id, setID] = useState('');
+    const search = async (event) => {
+        event.preventDefault();
+        if (id === "") {
+            window.location.reload();
+            return;
+        }
+        fetch(`/api/restaurantcustomers?restaurantID=${id}`)
+            .then(data => data.json())
+            .then(json => setTableData(json))
+            .catch(err => console.error(err));
+    }
+
     return (
         <>
             <SideBar/>
             <br/>
 
             <h1>RestaurantCustomers</h1>
-            <SearchRestaurantCustomers restaurantData={restaurantData}/>
+            <form onSubmit={search}>
+                <label htmlFor="restaurantcustomers">Search RestaurantCustomers</label>
+                <input
+                    type="text"
+                    placeholder="Enter restaurant ID"
+                    list="restaurantIDs"
+                    onChange={e => setID(e.target.value)}
+                ></input>
+                <RestaurantDataList restaurantData={restaurantData} id="restaurantIDs"/>
+                <button>Search</button>
+                <br/>
+            </form>
 
             <div className="table-container">
                 <RestaurantCustomersTable data={tableData}/>
