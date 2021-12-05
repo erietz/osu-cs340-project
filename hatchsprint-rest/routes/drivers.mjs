98034e0d37d3cc2401c6edf744989fb1ce8997ed
Ethan Rietz <ewrietz@gmail.com>
2021-12-05 13:31:45 -0600

    change status code of put request

--- hatchsprint-rest/routes/drivers.mjs
+++ hatchsprint-rest/routes/drivers.mjs
@@ -67,7 +67,7 @@ drivers.put("/", (req, res) => {
                 console.error(error);
                 res.status(400).json(error);
             } else {
-                res.status(201).json({ status: "Driver Updated" })
+                res.status(200).json({ status: "Driver Updated" })
             }
         });
     }
